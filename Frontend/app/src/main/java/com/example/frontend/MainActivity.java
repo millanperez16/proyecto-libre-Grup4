21334985bb0d536f4b6f733003cb2a26a2eaf2ac
dgr-85 <davidgomezrovira85@gmail.com>
2024-04-09 21:10:34 +0200

    Frontend formulario registro

--- Frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -16,11 +19,23 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView tvLoginRegister=findViewById(R.id.tvLoginRegister);
+        tvLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToRegisterMenu();
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
+    }
+
+    public void goToRegisterMenu(){
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
--- Frontend/app/src/main/java/com/example/frontend/RegisterActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        finish();
     }
 }
