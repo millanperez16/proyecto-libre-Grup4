0da5092ef0304676ba399461c7f53de7537bf638
millanperez16 <ismaelmillanperez@gmail.com>
2024-05-30 21:16:18 +0200

    Documentación

--- Frontend/app/src/main/java/com/example/frontend/impl/ApiServiceImpl.java
+++ Frontend/app/src/main/java/com/example/frontend/impl/ApiServiceImpl.java
@@ -2,13 +2,9 @@ package com.example.frontend.impl;
 
 
 import android.content.Context;
-<<<<<<< HEAD
-=======
-import android.security.keystore.KeyInfo;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
->>>>>>> 912099caa6b1e5a4c0363c8db492e8fd1fbb71d6
 
 import com.example.frontend.R;
 import com.example.frontend.interfaces.ApiService;
@@ -18,8 +14,6 @@ import java.security.KeyStore;
 import java.security.Provider;
 import java.security.SecureRandom;
 import java.security.Security;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
@@ -34,21 +28,16 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import org.bouncycastle.jcajce.PKCS12Key;
-import org.bouncycastle.jcajce.provider.keystore.PKCS12;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class ApiServiceImpl {
     private static ApiService apiService;
-<<<<<<< HEAD
     private static final String BASE_URL = "https://do.diba.cat/api/dataset/municipis/";
     private static final String BASE_URL2 = "https://gestorapi.gencat.cat/dadesobertes/consulta/consultadades";
     private static final String BASE_URL_REGISTER = "https://localhost:8442/clientes/";
-=======
     private static final String URL_MUNICIPIS = "https://do.diba.cat/api/dataset/municipis/";
     private static final String URL_REGISTER = "https://localhost:8442/clientes/";
     private static final String URL_LOGIN = "https://localhost:8442/authenticate/";
->>>>>>> 912099caa6b1e5a4c0363c8db492e8fd1fbb71d6
     private static Context contextApp;
 
     public static ApiService getApiServiceMunicipi(String like) {
@@ -127,10 +116,7 @@ public class ApiServiceImpl {
     public static OkHttpClient getClient(){
         try{
             InputStream inputStream = contextApp.getResources().openRawResource(R.raw.euroconstrucciones);
-<<<<<<< HEAD
 
-            KeyStore keyStore = KeyStore.getInstance("pkcs12");
-=======
             if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
                 Security.addProvider(new BouncyCastleProvider());
             }
@@ -138,7 +124,6 @@ public class ApiServiceImpl {
                 Log.d("Provider",p.toString());
             }
             KeyStore keyStore = KeyStore.getInstance("PKCS12",BouncyCastleProvider.PROVIDER_NAME);
->>>>>>> 912099caa6b1e5a4c0363c8db492e8fd1fbb71d6
             keyStore.load(inputStream,"euroconstrucciones".toCharArray());
 
             KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
@@ -148,12 +133,6 @@ public class ApiServiceImpl {
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             trustManagerFactory.init(keyStore);
             TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-<<<<<<< HEAD
-            X509TrustManager trustManager = null;
-            for (TrustManager tm : trustManagers){
-                if (tm instanceof X509TrustManager){
-                    trustManager = (X509TrustManager) tm;
-=======
 
             // Obtener el X509TrustManager del TrustManagerFactory
             X509TrustManager x509TrustManager = null;
@@ -161,7 +140,6 @@ public class ApiServiceImpl {
                 if (trustManager instanceof X509TrustManager) {
                     x509TrustManager = (X509TrustManager) trustManager;
                     break;
->>>>>>> 912099caa6b1e5a4c0363c8db492e8fd1fbb71d6
                 }
             }
 
@@ -169,13 +147,8 @@ public class ApiServiceImpl {
             sslContext.init(keyManagers, trustManagerFactory.getTrustManagers(), new SecureRandom());
 
             SSLSocketFactory socketFactory = sslContext.getSocketFactory();
-<<<<<<< HEAD
-            OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder().
-                    sslSocketFactory(socketFactory, trustManager)
-=======
             OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder()
                     .sslSocketFactory(socketFactory, x509TrustManager)
->>>>>>> 912099caa6b1e5a4c0363c8db492e8fd1fbb71d6
                     .hostnameVerifier((hostname, session) -> true);
 
             return clientBuilder.build();
